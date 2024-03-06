b557ec37e649f336698915db3ba906ad9c9892ce
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-03-06 13:28:03 -0500

    layout and navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/Nav/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <main className="max-w-7xl mx-auto lg:py-10 space-y-10 p-5 lg:p-0">
+              <Navbar />
+              {children}  
+            </main>
+          </ThemeProvider>
+      </body>
     </html>
   );
 }
