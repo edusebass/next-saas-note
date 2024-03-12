7b5422d1d03cd9243853d0e12b8bd1b5665178f0
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-03-12 13:59:34 -0500

    user proflie

--- components/Nav/Navbar.tsx
+++ components/Nav/Navbar.tsx
@@ -1,17 +1,23 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { use } from 'react'
 import { Button } from '../ui/button'
 import { FaGithub } from "react-icons/fa";
 import LoginForm from './LoginForm';
+import { useUser } from '@/lib/store/user';
+import Profile from './Profile';
 
 export default function Navbar() {
+  const user = useUser((state) => state.user)
+
   return (
     <nav className='flex items-center justify-between'>
         <div className='group'>
             <Link href="/" className='text-2xl font-bold'>Daily Blog</Link>
             <div className='h-1 w-0 group-hover:w-full transition-all bg-green-500'></div>
         </div>
-        <LoginForm />
+        {user ? <Profile/> : <LoginForm />}
+        
     </nav>
   )
 }
